692f963651424214a642328f57a758fd113dbb80
Desleyo <desleyoord@gmail.com>
2026-08-03 18:07:54 +0200

    Fix hierarchy

--- JavaScript/component.js
+++ JavaScript/component.js
@@ -4,5 +4,5 @@ async function loadComponent(id, file) {
     element.innerHTML = await response.text();
 }
 
-loadComponent("header", "/HTML/Components/header.html");
-loadComponent("footer", "/HTML/Components/footer.html");
+loadComponent("header", "Components/header.html");
+loadComponent("footer", "Components/footer.html");
